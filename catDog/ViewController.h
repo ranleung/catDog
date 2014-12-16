e42cf5452b2ec64799b3fe6993860b1e56e49e0a
ranleung <leungran@gmail.com>
2014-12-16 15:47:02 -0800

    Initial Commit

---
+++ catDog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  catDog
+//
+//  Created by Randall Leung on 12/16/14.
+//  Copyright (c) 2014 Randall Leung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ catDog/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  catDog
+//
+//  Created by Randall Leung on 12/16/14.
+//  Copyright (c) 2014 Randall Leung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
